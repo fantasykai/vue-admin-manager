08e85da6b1d89407977500af545a750b279d1e3e
fantasykai <makren@126.com>
2017-08-02 20:47:12 +0800

    报表支持统计汇总

--- src/components/page/operation/OperatingAccountManage.vue
+++ src/components/page/operation/OperatingAccountManage.vue
@@ -15,7 +15,7 @@
             </el-form>
         </el-col>
         <!--列表-->
-        <el-table :data="records" border highlight-current-row v-loading="listLoading"
+        <el-table :data="records" :summary-method="getSummaries" show-summary border highlight-current-row v-loading="listLoading"
                   style="width: 100%;">
             <!--<el-table-column type="selection" width="55">-->
             <!--</el-table-column>-->
@@ -135,6 +135,35 @@
                     this.records = res.data._items;
                     this.listLoading = false;
                 });
+            },
+            getSummaries(param) {
+                const {columns, data} = param;
+                const sums = [];
+                columns.forEach((column, index) => {
+                    if (index === 0) {
+                        sums[index] = '合计';
+                        return;
+                    }
+                    if (index === 1) {
+                        sums[index] = 'N/A';
+                        return;
+                    }
+                    const values = data.map(item => Number(item[column.property]));
+                    if (!values.every(value => isNaN(value))) {
+                        sums[index] = values.reduce((prev, curr) => {
+                            const value = Number(curr);
+                            if (!isNaN(value)) {
+                                return prev + curr;
+                            } else {
+                                return prev;
+                            }
+                        }, 0);
+                        sums[index] += '';
+                    } else {
+                        sums[index] = 'N/A';
+                    }
+                });
+                return sums;
             },
         },
         mounted() {
